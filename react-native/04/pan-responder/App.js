0dcd62a15c570e60e2c2911cc3ddbe36150f4682
양혜령 <hyeryung.yang@bankwareglobal.com>
2019-07-03 14:11:37 +0900

    add react-native-04-pan-responder

---
+++ react-native/04/pan-responder/App.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import PanDemo from './PanDemo';
+
+export default function App() {
+  return (
+    <PanDemo></PanDemo>
+  );
+}
